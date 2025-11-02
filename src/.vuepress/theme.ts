a05c582277df62eea84ad6d9abeb3baf24669e57
zhihuaiwen <1570631036@qq.com>
2025-11-03 01:05:11 +0800

    update

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -9,6 +9,7 @@ export default hopeTheme({
         hostname: "https://www.wenzhihuai.com/",
         logo: "https://github-images.wenzhihuai.com/images/logo.png",
         favicon: "/favicon.ico",
+        sidebarSorter: ["order", "date-desc"],
         // iconAssets: "//at.alicdn.com/t/c/font_2922463_o9q9dxmps9.css",
         markdown: {
             align: true,
